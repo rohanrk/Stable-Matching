bfeae4228e732dffd4ba840043c3649d84921ced
rohanrk <rohanrk2009@gmail.com>
2019-09-16 13:44:49 -0400

    Update formatting

--- match.py
+++ match.py
@@ -8,7 +8,6 @@ default_little_filename = "LittleListChoices.csv"
 # Static variables
 matches = dict()
 matched_littles = set()
-rem_pledges = []
 
 """ Function takes in a file names and completes set of all name and preferences dictionary
 
@@ -16,17 +15,20 @@ Assuming csvs are in the following format
 
 big_list:
 
-pledge_name, first_big_choice, second_big_choice, ...
+pledge_name,first_big_choice,second_big_choice,...
 
 little_list:
 
-bro_name, first_little_choice, second_little_choice, ...
+bro_name,first_little_choice,second_little_choice,...
+
+Make sure there are no spaces between the commas or this will break
 """
 def process_file(name):
     all_names = set() # All pledge names or all bros names (who filled ou the form)
     preferences = dict()
     with open(name, mode='r') as f:
         for line in f:
+            line = line.strip('\n')
             choices = choices = line.split(",")
             preferences[choices[0]] = choices[1:]
     return preferences    
@@ -38,7 +40,7 @@ def match(big_prefs, little_prefs):
         small = choice(rem_pledges)
         prefs = big_prefs[small]
         rem_pledges.remove(small)
-        big, little, little_index = _match(small, prefs, little_prefs)
+        big, little, little_index = _match(small, prefs, little_prefs, rem_pledges)
         matches[big] = (little, little_index)
             
     return matches
@@ -48,17 +50,17 @@ all bros little preferences
 
 Returns a tuple (matched big, matched little)
 """
-def _match(little, prefs, little_prefs):
+def _match(little, prefs, little_prefs, rem_pledges):
     for big in prefs:
         if big in little_prefs:
-            # Big has already been matched. Check if big prefers little over other match
+            little_index = sys.maxsize # Very large number to indiciate that little is not in big's list
+            if little in little_prefs[big]:
+                little_index = little_prefs[big].index(little)
             if big in matches:
-                little_index = -1
-                if little in little_prefs[big]:
-                    little_index = little_prefs[big].index(little)
+                # Big has already been matched. Check if big prefers little over other match
                     
                 old_little = matches[big][0]
-                if little_index > matches[big][1]:
+                if little_index < matches[big][1]:
                     matched_littles.remove(old_little)
                     rem_pledges.append(old_little)
                     matched_littles.add(little)
@@ -75,12 +77,12 @@ def _match(little, prefs, little_prefs):
 if __name__=='__main__':
     # If you don't specify csv file names we assume your big list is "BigListChoices.csv"
     # and your little list is "LittleListChoices.csv"
-    if len(sys.argv) == 1 or len(sys.argv) > 2:
-        print("Incorrect number of arguments. Run python3 match.py <bigs_file_name> <littles_file_name>\n,
-            "if your filenames are titled `BigListChoices.csv` and `LittleListChoices.csv` you can just run python3 match.py")
+    if len(sys.argv) > 3 or len(sys.argv) == 2:
+        print("Incorrect number of arguments. Run python3 match.py <bigs_file_name> <littles_file_name>\nif your filenames are titled `BigListChoices.csv` and `LittleListChoices.csv` you can just run python3 match.py")
+        sys.exit()
     
-    big_prefs = process_file(default_big_filename) if not sys.argv else process_file(sys.argv[0])
-    little_prefs = process_file(default_little_filename) if not sys.argv else process_file(sys.argv[1])
+    big_prefs = process_file(default_big_filename) if len(sys.argv) == 1 else process_file(sys.argv[1])
+    little_prefs = process_file(default_little_filename) if len(sys.argv) == 1 else process_file(sys.argv[2])
     
     
     pairings = match(big_prefs, little_prefs)
@@ -100,7 +102,7 @@ if __name__=='__main__':
     with open("results.txt", mode='w') as f:
         print("Results are also written out to `results.txt`")
         for big, little in matches.items():
-            out = "Pledge: {} Big: {}".format(little, big)
-            f.write(out, "\n")
+            out = "Pledge: {} Big: {}".format(little[0], big)
+            f.write("{}\n".format(out))
             print(out)
     
